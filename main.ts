080f387bcbc66c2873435a3a0549c5688287b430
audrey-sun-99 <audrey.sun@stu.ocsb.ca>
2026-03-06 12:38:34 -0500

    made a traffic light

--- main.ts
+++ main.ts
@@ -5,4 +5,30 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
